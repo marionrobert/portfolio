07964b408eda1db9050877b518cd23e99fb34c7a
Marion Robert <mrobert148@gmail.com>
2024-06-28 17:45:46 -0400

    render all projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,9 +4,10 @@ interface ProjectProps {
   technologies: string[];
   features: string[];
   images: string[];
+  link: string;
 }
 
-const ProjectComponent: React.FC<ProjectProps> = ({ title, description, technologies, features, images }) => {
+const ProjectComponent: React.FC<ProjectProps> = ({ title, description, technologies, features, images, link }) => {
   return (
     <article className="project">
       <h2>{title}</h2>
@@ -17,22 +18,25 @@ const ProjectComponent: React.FC<ProjectProps> = ({ title, description, technolo
             <li key={index}>{tech}</li>
           ))}
         </ul>
-        <p>Fonctionnalités principales :</p>
-        <ul>
+        <h3>Fonctionnalités principales :</h3>
+        <ul className="features">
         {features.map((feature, index) => (
             <li key={index}>{feature}</li>
           ))}
         </ul>
+        <a className="btn btn-dark mt-4" href={link} target="_blank">Voir le code</a>
       </div>
       <div className="image-layout">
-        <div className="big-image">
+        {/* <div className="big-image">
           <img src={images[0]} alt="Harmony 1" />
         </div>
         <div className="small-images">
           <img src={images[1]} alt="Harmony 2" />
           <img src={images[2]} alt="Harmony 3" />
-        </div>
+        </div> */}
+        <img className="image" src={images[0]}/>
       </div>
+
     </article>
   )
 }
--- src/components/ProjectsContainer.tsx
+++ src/components/ProjectsContainer.tsx
@@ -1,5 +1,6 @@
 import Project from "./Project";
-import { harmony1, harmony2, harmony3 } from "../assets/images/projects";
+import { harmony1, harmony2, harmony3, verdure1, verdure2, verdure3, synopsix1, synopsix2, synopsix3, waw, waw1, waw2, waw3, guitarpick1, guitarpick2, guitarpick3 } from "../assets/images/projects";
+
 
 const projects = [
   {
@@ -26,7 +27,109 @@ const projects = [
       'Modération des activités et commentaires par un administrateur',
       "Notification de l'utilisateur par emails"
     ],
-    images: [harmony1, harmony2, harmony3]
+    images: [harmony1, harmony2, harmony3],
+    link: "https://github.com/marionrobert/harmony-front-react"
+  },
+  {
+    title: "Verdure",
+    description: "Verdure est une application web de vente en ligne de plantes d'intérieur. L'application est disponible uniquement en français.",
+    technologies: [
+      'JavaScript',
+      'NodeJS',
+      'Express',
+      'React',
+      'SQL / MySQL',
+      'Redux',
+      'Axios',
+      'bcrypt',
+      'Stripe',
+      'CSS / SCSS'
+    ],
+    features: [
+      'Gestion des sessions utilisateurs',
+      'Gestion du panier',
+      'Gestion du paiement avec stripe',
+      'Dashboard administrateur',
+      'Modération des activités et commentaires par un administrateur',
+      "Suivi de l'état commandes"
+    ],
+    images: [verdure1, verdure2, verdure3],
+    link: "https://github.com/marionrobert/verdure-front-react"
+  },
+  {
+    title: "Synopsix",
+    description: "Synopsix est une appplication web proposant un jeu autour de l'univers du cinéma, développé sur le modèle de Pédantix. Le but du jeu est de retrouver le titre du film en découvrant peu à peu le synopsix de ce dernier. Deux modes sont possible. Le mode classique (Play word search / input mode) : il s'agit d'entrer des mots pour découvrir progressivement le synopsix. Lle mode contre la montre (Play Against Time / timer mode) : vous avez deux minutes pour retrouver le titre du film dont les mots du synopsix apparaissent progressivement.",
+    technologies: [
+      'Ruby',
+      'Rails',
+      'PostgreSQL',
+      'Devise',
+      'Cloudinary',
+      'Bootstrap'
+    ],
+    features: [
+      'Gestion des sessions utilisateurs',
+      'Gestion du panier',
+      'Gestion du paiement avec stripe',
+      'Dashboard administrateur',
+      'Modération des activités et commentaires par un administrateur',
+      "Suivi de l'état commandes"
+    ],
+    images: [synopsix1, synopsix2, synopsix3],
+    link: "https://github.com/marionrobert/synopsix-940"
+  },
+  {
+    title: "GuitarPick",
+    description: "Guitar Pick est un site de location de guitares, développé sur le modèle d'AirBnb. L'application est disponible uniquement en anglais.",
+    technologies: [
+      'Ruby',
+      'Rails',
+      'PostgreSQL',
+      'Devise',
+      'Cloudinary',
+      'Bootstrap'
+    ],
+    features: [
+      'Gestion des sessions utilisateurs',
+      'Gestion du panier',
+      'Gestion du paiement avec stripe',
+      'Dashboard administrateur',
+      'Modération des activités et commentaires par un administrateur',
+      "Suivi de l'état commandes"
+    ],
+    images: [guitarpick1, guitarpick2, guitarpick3],
+    link: "https://github.com/marionrobert/GUITAR_PICK"
+  },
+  {
+    title: "Wall Art Works",
+    description: "Commencé comme un projet visant à créer un modèle e-commerce personnalisable, ce projet a évolué pour devenir un site web marchand spécialisé dans la vente de tableaux générés par intelligence artificielle.",
+    technologies: [
+      'Ruby',
+      'Rails',
+      'PostgreSQL',
+      'Devise',
+      "Pagy",
+      "Omniauth",
+      "pP_search",
+      'Cloudinary',
+      'Rails_admin',
+      "Ransack"
+    ],
+    features: [
+      "CRUD Complet",
+      "Hébergement d'images dans le cloud",
+      "Authentification (notamment via OmniAuth Google Facebook)",
+      "Stripe",
+      "Barre de recherche",
+      "Tableau de bord administrateur",
+      "Formulaire de contact",
+      "Géolocalisation",
+      "Previsualisation virtuelle d'environnement",
+      "Gestion du panier",
+      "Interface de paiement externalisé avec stripe"
+    ],
+    images: [waw],
+    link: "https://github.com/marionrobert/GUITAR_PICK"
   }
 ]
 
@@ -42,6 +145,7 @@ export default function ProjectsContainer() {
           technologies={project.technologies}
           features={project.features}
           images={project.images}
+          link={project.link}
         />
       ))}
     </>
