69bbf737f85a7c47907a5bb281aae47a3b47c47b
Marion Robert <mrobert148@gmail.com>
2024-07-03 16:24:20 -0400

    footer ok

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+export default function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a aria-label="Icône de LinkedIn contenant un lien vers la page LinkedIn du créateur du site" href="https://www.linkedin.com/in/marion-robert-" target="_blank" rel='noreferrer'><FontAwesomeIcon icon={faLinkedin} className="icon"/></a>
+        </li>
+        <li>
+          <a aria-label="Icône de Github contenant un lien vers la page Github du créateur du site" href="https://github.com/marionrobert" target="_blank" rel='noreferrer'><FontAwesomeIcon icon={faGithub} className="icon" /></a>
+        </li>
+      </ul>
+      <div>
+        <p className="brand">MR</p>
+      </div>
+      <p>© 2024, Portfolio Marion Robert</p>
+    </footer>
+  )
+}
