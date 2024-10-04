b409a93029d7e605bfbf83c7ef9499f340499050
Marion Robert <mrobert148@gmail.com>
2024-10-04 13:19:44 -0400

    translation for contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,22 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLocationDot, faSquarePhone, faAt, faArrowRight } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faSquarePhone, faAt, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
 
 export default function Contact() {
+  const {t} = useTranslation();
+
   return (
     <section id="contact">
-      <h2>Contactez-moi</h2>
+      <h2>{t("contact.title")}</h2>
       <ul>
         <li><FontAwesomeIcon icon={faAt} /> mrobert148@gmail.com</li>
         <li><FontAwesomeIcon icon={faSquarePhone} /> (+1)438-812-7844</li>
-        <li><FontAwesomeIcon icon={faLocationDot} /> Montréal, Québec, Canada</li>
+        <li><FontAwesomeIcon icon={faLocationDot} />{t("contact.city")}</li>
       </ul>
       <a className="cta" target="_blank" href="mailto:mrobert148@gmail.com?subject=Prise de contact">
-          Envoyer un email <FontAwesomeIcon icon={faArrowRight} />
+        {t("contact.button")}
+        <FontAwesomeIcon icon={faArrowRight} />
       </a>
-
     </section>
   )
 }
