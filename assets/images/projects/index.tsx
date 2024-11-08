850cc0f7f953fe6a16524008bc04c321e6aad9b8
Marion Robert <mrobert148@gmail.com>
2024-11-08 11:44:55 -0500

    Updates

---
+++ assets/images/projects/index.tsx
@@ -0,0 +1,14 @@
+// Exports for guitarpick images
+export { default as guitarpick } from "./guitarpick/guitarpick.gif";
+
+// Exports for harmony images
+export { default as harmony } from "./harmony/harmony.gif";
+
+// Exports for synopsix images
+export { default as synopsix } from "./synopsix/synopsix.gif";
+
+// Exports for verdure images
+export { default as verdure } from "./verdure/verdure.gif";
+
+// Exports for waw images
+export { default as waw} from "./waw/waw.gif";
